3b5cbdd70804e98d38e1611cb40e2c9abf529578
Kyzyl Monteiro <kyzylmonteiro@gmail.com>
2020-08-12 00:25:48 +0530

    Updates

---
+++ precache-manifest.3b464b3cd42cc0ff622d81edbe29f024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580f070561fd1c83e15d272283cfc0c9",
+    "url": "/jiv/index.html"
+  },
+  {
+    "revision": "12504079c4fa1740068a",
+    "url": "/jiv/static/css/main.3b3ddbdb.chunk.css"
+  },
+  {
+    "revision": "60bce2c01cfe532eee28",
+    "url": "/jiv/static/js/2.8bdf9674.chunk.js"
+  },
+  {
+    "revision": "07e311592af5ffd68ba8fef2ec98e644",
+    "url": "/jiv/static/js/2.8bdf9674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12504079c4fa1740068a",
+    "url": "/jiv/static/js/main.07168acf.chunk.js"
+  },
+  {
+    "revision": "0a1156b95acd4d4dfc0b",
+    "url": "/jiv/static/js/runtime-main.8bed18fd.js"
+  }
+]);
